d41d366f5a8d918abb377bae7522a03585cf1a9d
Omuriev <oleguer132@gmail.com>
2024-08-08 21:22:39 +0300

    Изменена логика в классе MainApplication

--- src/main/java/com/stepup/proje/MainApplication.java
+++ src/main/java/com/stepup/proje/MainApplication.java
@@ -4,5 +4,7 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("1111");
+        System.out.println("2");
+        System.out.println("4");
     }
 }
