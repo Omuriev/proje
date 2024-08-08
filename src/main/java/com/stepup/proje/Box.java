fd3f73ac5854129c371890d0d58720ae4856e963
Omuriev <oleguer132@gmail.com>
2024-08-08 21:21:23 +0300

    Добавлен класс коробки

---
+++ src/main/java/com/stepup/proje/Box.java
@@ -0,0 +1,17 @@
+package com.stepup.proje;
+
+public class Box {
+    private int size;
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Box(int size) {
+        this.size = size;
+    }
+}
