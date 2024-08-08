c9f5f1a344bda29e44fd3c024007c497ff2ebdbd
Omuriev <oleguer132@gmail.com>
2024-08-08 21:29:37 +0300

    Изменили логику main

--- src/main/java/com/stepup/proje/MainApplication.java
+++ src/main/java/com/stepup/proje/MainApplication.java
@@ -3,8 +3,9 @@ package com.stepup.proje;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("----");
         System.out.println("1111");
-        System.out.println("2");
+        System.out.println("1");
         System.out.println("4");
     }
 }
